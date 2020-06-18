1d424cdad7b7698811c1528f6548921587ff8585
unknown <VincentHui@hotmail.co.uk>
2020-06-18 16:51:00 +0100

    switching to splat and making cards more reactive and a little shorter

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@ import * as serviceWorker from './serviceWorker';
 import './index.css'
 
 const cards = [
-  'https://dvl-gng.s3.eu-west-2.amazonaws.com/616.png',
-  'https://dvl-gng.s3.eu-west-2.amazonaws.com/GNG.png',
-  'https://dvl-gng.s3.eu-west-2.amazonaws.com/DVL.png',
-  'https://dvl-gng.s3.eu-west-2.amazonaws.com/dvlGng616.png',
-  'https://dvl-gng.s3.eu-west-2.amazonaws.com/FIRST.png'
+  'https://dvl-gng.s3.eu-west-2.amazonaws.com/616-splat.png',
+  'https://dvl-gng.s3.eu-west-2.amazonaws.com/GNG-splat.png',
+  'https://dvl-gng.s3.eu-west-2.amazonaws.com/DVL-splat.png',
+  'https://dvl-gng.s3.eu-west-2.amazonaws.com/dvlGng616-splat.png',
+  'https://dvl-gng.s3.eu-west-2.amazonaws.com/first-splat.png'
 ]
 
 // These two are just helpers, they curate spring data, values that are later being interpolated into css
-const to = i => ({ x: 0, y: i * -4, scale: 1, rot: -10 + Math.random() * 30, delay: i * 100 })
-const from = i => ({ x: 0, rot: 0, scale: 1.7, y: -1500 })
+const to = i => ({ x: 0, y: i * -4, scale: 1, rot: -15 + Math.random() * 50, delay: i * 100 })
+const from = i => ({ x: 0, rot: 0, scale: 2.7, y: -1500 })
 // This is being used down there in the view, it interpolates rotation and scale into a css transform
 const trans = (r, s) => `perspective(1500px) rotateX(30deg) rotateY(${r / 10}deg) rotateZ(${r}deg) scale(${s})`
 
@@ -31,8 +31,8 @@ function Deck() {
       if (index !== i) return // We're only interested in changing spring-data for the current spring
       const isGone = gone.has(index)
       const x = isGone ? (200 + window.innerWidth) * dir : down ? xDelta : 0 // When a card is gone it flys out left or right, otherwise goes back to zero
-      const rot = xDelta / 100 + (isGone ? dir * 10 * velocity : 0) // How much the card tilts, flicking it harder makes it rotate faster
-      const scale = down ? 1.1 : 1 // Active cards lift up a bit
+      const rot = xDelta / 100 + (isGone ? dir * 70 * velocity : 0) // How much the card tilts, flicking it harder makes it rotate faster
+      const scale = down ? 1.2 : 1 // Active cards lift up a bit
       return { x, rot, scale, delay: undefined, config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 } }
     })
     if (!down && gone.size === cards.length) setTimeout(() => gone.clear() || set(i => to(i)), 600)
